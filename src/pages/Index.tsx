8c836cca794693847cbb8004d7400f11cc3ca141
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-09 18:36:12 +0530

    Add src/pages/Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { TodoContainer } from "@/components/todo/todo-container";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <TodoContainer />
+    </div>
+  );
+};
+
+export default Index;
