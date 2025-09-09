55b4d9237dd8539906ff1f59e86f866247387529
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-09 18:34:45 +0530

    Add src/components/todo/todo-list.tsx

---
+++ src/components/todo/todo-list.tsx
@@ -0,0 +1,35 @@
+import { TodoItem } from "@/components/todo/todo-item";
+import { Todo } from "@/types/todo";
+
+interface TodoListProps {
+  todos: Todo[];
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
+  onEdit: (id: string, newText: string) => void;
+}
+
+export const TodoList = ({ todos, onToggle, onDelete, onEdit }: TodoListProps) => {;
+  if (todos.length === 0) {
+    return (
+      <div className="text-center py-8 text-gray-500">
+        No tasks yet. Add one above!
+      </div>
+    );
+  }
+
+  return (
+    <div className="space-y-2">
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          completed={todo.completed}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
+      ))}
+    </div>
+  );
+}
